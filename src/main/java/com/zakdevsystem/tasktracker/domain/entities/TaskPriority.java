3907e639522f4aa152d74e44a2dd4983994840fd
MyDevSystem <rawaha.coder@gmail.com>
2025-08-14 23:10:39 +0100

    create Task entity

---
+++ src/main/java/com/zakdevsystem/tasktracker/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.zakdevsystem.tasktracker.domain.entities;
+
+public enum TaskPriority {
+    HIGH, MEDIUM, LOW
+}
---
+++ src/main/java/com/zakdevsystem/tasktracker/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.zakdevsystem.tasktracker.domain.entities;
+
+public enum TaskStatus {
+    OPEN, CLOSE
+}
