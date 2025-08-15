4dccdc26e25b5297675f8dce551830223bd64bb6
MyDevSystem <rawaha.coder@gmail.com>
2025-08-15 09:08:38 +0100

    create task and tasklist repositories

---
+++ src/main/java/com/zakdevsystem/tasktracker/repositories/TaskListRepository.java
@@ -0,0 +1,11 @@
+package com.zakdevsystem.tasktracker.repositories;
+
+import com.zakdevsystem.tasktracker.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+}
---
+++ src/main/java/com/zakdevsystem/tasktracker/repositories/TaskRepository.java
@@ -0,0 +1,17 @@
+package com.zakdevsystem.tasktracker.repositories;
+
+import com.zakdevsystem.tasktracker.domain.entities.Task;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+    List<Task> findByTaskListId(UUID taskListId);
+
+    Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+
+}
