7b02454759fb84ce42b0db246e4cab83dcef0256
MyDevSystem <rawaha.coder@gmail.com>
2025-08-15 18:42:33 +0100

    add createTask methode to TaskService

--- src/main/java/com/zakdevsystem/tasktracker/controllers/TaskController.java
+++ src/main/java/com/zakdevsystem/tasktracker/controllers/TaskController.java
@@ -1,12 +1,10 @@
 package com.zakdevsystem.tasktracker.controllers;
 
 import com.zakdevsystem.tasktracker.domain.dto.TaskDto;
+import com.zakdevsystem.tasktracker.domain.entities.Task;
 import com.zakdevsystem.tasktracker.mapper.TaskMapper;
 import com.zakdevsystem.tasktracker.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -29,5 +27,16 @@ public class TaskController {
                 .stream()
                 .map(taskMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskDto taskDto){
+
+        Task createdTask = taskService.createTask(taskListId, taskMapper.fromDto(taskDto));
+
+        return taskMapper.toDto(createdTask);
+
     }
 }
--- src/main/java/com/zakdevsystem/tasktracker/services/impl/TaskServiceImpl.java
+++ src/main/java/com/zakdevsystem/tasktracker/services/impl/TaskServiceImpl.java
@@ -1,24 +1,65 @@
 package com.zakdevsystem.tasktracker.services.impl;
 
 import com.zakdevsystem.tasktracker.domain.entities.Task;
+import com.zakdevsystem.tasktracker.domain.entities.TaskList;
+import com.zakdevsystem.tasktracker.domain.entities.TaskPriority;
+import com.zakdevsystem.tasktracker.domain.entities.TaskStatus;
+import com.zakdevsystem.tasktracker.repositories.TaskListRepository;
 import com.zakdevsystem.tasktracker.repositories.TaskRepository;
 import com.zakdevsystem.tasktracker.services.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskListRepository taskListRepository;
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, TaskListRepository taskListRepository) {
         this.taskRepository = taskRepository;
+        this.taskListRepository = taskListRepository;
     }
 
     @Override
     public List<Task> listTasks(UUID taskListId) {
         return taskRepository.findByTaskListId(taskListId);
+    }
+
+    @Override
+    public Task createTask(UUID taskListId, Task task) {
+        if (null != task.getId()){
+            throw new IllegalArgumentException("Task already has an ID");
+        }
+        if (null == task.getTitle() || task.getTitle().isBlank()){
+            throw new IllegalArgumentException("Task must have a title");
+        }
+
+        TaskPriority taskPriority = Optional.ofNullable(task.getPriority())
+                .orElse(TaskPriority.MEDIUM);
+
+        TaskStatus taskStatus = TaskStatus.OPEN;
+
+        TaskList taskList = taskListRepository.findById(taskListId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid task list ID provided"));
+
+        LocalDateTime timeNow = LocalDateTime.now();
+        Task taskToSave = new Task(
+                null,
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                taskStatus,
+                taskPriority,
+                timeNow,
+                timeNow,
+                taskList
+        );
+
+        return taskRepository.save(taskToSave);
     }
 }
